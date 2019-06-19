1220350a28632ee55c1e90409bc443deb7d3f641
Ulugbek <36325883+gkwd@users.noreply.github.com>
2019-06-19 19:56:09 +0500

    upfate

--- izattilla/js/script.js
+++ izattilla/js/script.js
@@ -16,43 +16,36 @@ let descTranslateHeight = 0;
 let btnPrev = document.getElementById('prev');
 let btnNext = document.getElementById('next');
 
-function nextBtn(){
-    if(slideNow == slideCount || slideNow <= 0 || slideNow >= slideCount){
-        sliderWrapper.style.transform = 'translate(0,0)';
-        descWrapper.style.transform = 'translate(0,0)';
-        slideNow = 1;
-      }
-      else{
-        translateWidth = -viewportWidth * slideNow;
-        descTranslateHeight = -descViewportHeight * slideNow;
-        sliderWrapper.style.transform = 'translate('+ translateWidth + 'px,0)';
-        descWrapper.style.transform = 'translate(0,'+ descTranslateHeight +'px)';
-        slideNow++;
-      }
+function nextBtn() {
+  if (slideNow == slideCount || slideNow <= 0 || slideNow >= slideCount) {
+    sliderWrapper.style.transform = 'translate(0,0)';
+    descWrapper.style.transform = 'translate(0,0)';
+    slideNow = 1;
+  } else {
+    translateWidth = -viewportWidth * slideNow;
+    descTranslateHeight = -descViewportHeight * slideNow;
+    sliderWrapper.style.transform = 'translate(' + translateWidth + 'px,0)';
+    descWrapper.style.transform = 'translate(0,' + descTranslateHeight + 'px)';
+    slideNow++;
+  }
 }
-function prevBtn(){
-    if(slideNow == 1 || slideNow <= 0 || slideNow > slideCount){
-        translateWidth = -viewportWidth * (slideCount - 1);
-        descTranslateHeight = -descViewportHeight * (slideCount - 1);
-        sliderWrapper.style.transform = 'translate('+ translateWidth +'px, 0)';
-        descWrapper.style.transform = 'translate(0,'+ descTranslateHeight +'px)';
-        slideNow = slideCount
 
-      }
-      else{
-        translateWidth = -viewportWidth * (slideNow - 2);
-        descTranslateHeight = -descViewportHeight * (slideNow - 2);
-        sliderWrapper.style.transform = 'translate('+ translateWidth +'px, 0)';
-        descWrapper.style.transform = 'translate(0,'+ descTranslateHeight +'px)';
-        slideNow--;
-        console.log(slideNow)
-      }
+function prevBtn() {
+  if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+    translateWidth = -viewportWidth * (slideCount - 1);
+    descTranslateHeight = -descViewportHeight * (slideCount - 1);
+    sliderWrapper.style.transform = 'translate(' + translateWidth + 'px, 0)';
+    descWrapper.style.transform = 'translate(0,' + descTranslateHeight + 'px)';
+    slideNow = slideCount
+
+  } else {
+    translateWidth = -viewportWidth * (slideNow - 2);
+    descTranslateHeight = -descViewportHeight * (slideNow - 2);
+    sliderWrapper.style.transform = 'translate(' + translateWidth + 'px, 0)';
+    descWrapper.style.transform = 'translate(0,' + descTranslateHeight + 'px)';
+    slideNow--;
+  }
 }
 btnPrev.addEventListener('click', prevBtn)
 
 btnNext.addEventListener('click', nextBtn)
-
-
-
-
-
