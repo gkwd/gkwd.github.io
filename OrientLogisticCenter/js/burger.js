9c78dd6eda38a1d03690f3deb24427a045e03f5a
Ulugbek <36325883+gkwd@users.noreply.github.com>
2019-06-30 23:44:22 +0500

    asghf

--- OrientLogisticCenter/js/burger.js
+++ OrientLogisticCenter/js/burger.js
@@ -4,5 +4,12 @@ const nav = document.querySelector('.navigation')
 burger.addEventListener('click', function(){
   burger.classList.toggle('avtive');
   nav.classList.toggle('avtive_mobile_nav');
+})
 
+
+const lang = document.getElementById('language');
+const drop = document.getElementById('lang1');
+lang.addEventListener('click', function(){
+  drop.classList.toggle('active_drop');
+  // drop.style.transform = 'scaleY(1)'
 })
