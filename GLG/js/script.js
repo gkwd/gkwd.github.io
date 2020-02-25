41fc7a6fd5b562bddc28b7ad3084fddf760c3a15
Ulugbek <36325883+gkwd@users.noreply.github.com>
2020-02-25 15:55:08 +0500

    some changes

--- GLG/js/script.js
+++ GLG/js/script.js
@@ -1,14 +1,16 @@
-var backToTop = document.getElementById("back-to-top");
+document.addEventListener("DOMContentLoaded", () => {
+    var backToTop = document.getElementById("back-to-top");
 
-window.addEventListener("scroll", (e) => {
-    if (window.scrollY > 300) {
-        backToTop.style.display = "flex";
-    } else {
-        backToTop.style.display = "none"
-    }
-});
+    window.addEventListener("scroll", (e) => {
+        if (window.scrollY > 300) {
+            backToTop.style.display = "flex";
+        } else {
+            backToTop.style.display = "none"
+        }
+    });
 
-backToTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.scrollTo(0, 0)
+    backToTop.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.scrollTo(0, 0)
+    })
 })
