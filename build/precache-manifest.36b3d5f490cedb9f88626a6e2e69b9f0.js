66e19c42d362a8f245dc2b04c9ca4785026bfd22
Ulugbek <36325883+gkwd@users.noreply.github.com>
2020-02-25 13:31:40 +0500

    build version testing

---
+++ build/precache-manifest.36b3d5f490cedb9f88626a6e2e69b9f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abd58e8de4516f70405ae800dbdd243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56ed7aa68361693d075e",
+    "url": "/static/css/main.c25b5800.chunk.css"
+  },
+  {
+    "revision": "34bcd1fe5390cfdc15df",
+    "url": "/static/js/2.68b5bf82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.68b5bf82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ed7aa68361693d075e",
+    "url": "/static/js/main.7c8fbdc5.chunk.js"
+  },
+  {
+    "revision": "5e8ec743c75f56454690",
+    "url": "/static/js/runtime-main.610cb9d3.js"
+  },
+  {
+    "revision": "24c52d013f5b68fed783f94bab5965b3",
+    "url": "/static/media/BG_Fon.24c52d01.png"
+  },
+  {
+    "revision": "eebf2894b069d9cb93a29dab12aa31f5",
+    "url": "/static/media/SFProDisplay-Light.eebf2894.ttf"
+  },
+  {
+    "revision": "6987bcc482500f459516dc0342836ee5",
+    "url": "/static/media/SFProDisplay-Regular.6987bcc4.ttf"
+  },
+  {
+    "revision": "455c86e27c39de3f9d3a21610492f03d",
+    "url": "/static/media/SFProDisplay-RegularItalic.455c86e2.ttf"
+  }
+]);
