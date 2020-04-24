6d1d3e3c93aed4c32ea1b3f2ea507061fdb95cff
Ulugbek Ibragimov <36325883+gkwd@users.noreply.github.com>
2020-04-24 17:05:52 +0500

    minor fix

--- GLG/js/script.js
+++ GLG/js/script.js
@@ -1,16 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var backToTop = document.getElementById("back-to-top");
+  var backToTop = document.getElementById("back-to-top");
 
-    window.addEventListener("scroll", (e) => {
-        if (window.scrollY > 300) {
-            backToTop.style.display = "flex";
-        } else {
-            backToTop.style.display = "none"
-        }
-    });
+  window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 300) {
+      backToTop.style.display = "flex";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
 
-    backToTop.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.scrollTo(0, 0)
-    })
-})
+  backToTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  });
+
+  const burgerBlock = document.getElementById("burger");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+
+  burgerBlock.addEventListener("click", function () {
+    burger.classList.toggle("active_burger");
+    nav.classList.toggle("avtive_mobile_nav");
+  });
+
+  // var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+
+  // make position sensitive to size and document's width
+  // var posx = (Math.random() * (document.width - divsize)).toFixed();
+  // var posy = (Math.random() * (document.height - divsize)).toFixed();
+
+  const lines = document.getElementById("lines");
+
+  for (let index = 0; index < 25; index++) {
+    var divsize = (Math.random() * 100 + 50).toFixed();
+    var posx = (Math.random() * (document.body.clientWidth  - divsize)).toFixed();
+    console.table(divsize, posx);
+    
+    const line = document.createElement("div");
+    line.classList.add("line");
+    line.style.position = 'absolute';
+    line.style.left = posx + 'px';
+
+    lines.appendChild(line);
+    console.log(line);
+  }
+});
