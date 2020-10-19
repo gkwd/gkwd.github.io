a9a1d8607e6e53520db406ce6f76417f7c5d2fb4
gkwd <int.hardworker@gmail.com>
2020-10-19 17:08:08 +0500

    some fix

--- evote/js/script.auth.js
+++ evote/js/script.auth.js
@@ -1,10 +1,5 @@
 const buttons = document.querySelectorAll(".choose-auth");
 console.log(buttons);
-// const forms = {
-//     oneid: 'oneid',
-//     registration: 'reg',
-//     login: 'login'
-// }
 const body = document.body;
 const forms = ["oneid", "reg", "login"];
 let curForm;
@@ -16,40 +11,13 @@ for (let index = 0; index < buttons.length; index++) {
     }
     buttons[index].classList.add("active");
 
-    body.setAttribute("form-name", forms[index]);
-    if (curForm != undefined || curForm != null) {
-      curForm.style.display = "none";
-    } else {
+    // body.setAttribute("form-name", forms[index]);
+    
+      if (curForm != undefined || curForm != null) {
+        curForm.style.display = "none";
+      }  
       curForm = document.getElementById(forms[index]);
-    }
-
-    if (curForm !== undefined || curForm !== null) {
       curForm.style.display = "flex";
-    }
+    // }
   });
 }
-
-function showSection(evt, sectionId) {
-  let i, tabSection, tabBtn;
-
-  tabSection = document.getElementsByClassName("content-list");
-  for (i = 0; i < tabSection.length; i++) {
-    tabSection[i].style.display = "none";
-  }
-
-  tabBtn = document.getElementsByClassName("tab-btn");
-  for (i = 0; i < tabBtn.length; i++) {
-    tabBtn[i].classList.remove("active");
-  }
-
-  document.getElementById(sectionId).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-const burger = document.getElementById("burger");
-const nav = document.querySelector("nav");
-
-burger?.addEventListener("click", function () {
-  burger.classList.toggle("active_burger");
-  nav.classList.toggle("open");
-});
