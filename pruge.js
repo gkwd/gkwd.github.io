0f74225f03d1b46ac8dba836245f184fe53e63dd
Ulugbek Ibragimov <kirkland@kirkland.local>
2025-06-07 21:33:42 +0500

    del dsstore

--- pruge.js
+++ pruge.js
@@ -1,10 +1,20 @@
 import { PurgeCSS } from 'purgecss';
+import CleanCSS from 'clean-css';
+import fs from 'node:fs';
 
 const purgeCSSResult = await new PurgeCSS().purge({
   content: ['projects/**/*.html', 'projects/**/*.js'],
   css: ['projects/**/*.css'],
 });
 
-purgeCSSResult.forEach((res) =>
-  console.log('Status for: ', res.file, res.rejected, res.rejectedCss)
-);
+purgeCSSResult.forEach((res) => {
+  console.log('Status for: ', res.file, res.rejected, res.rejectedCss);
+
+  try {
+    const minifiedCss = new CleanCSS().minify(res.css);
+    fs.writeFileSync(res.file, minifiedCss.styles, { encoding: 'utf-8' });
+    console.log('File written successfully');
+  } catch (err) {
+    console.error(err);
+  }
+});
